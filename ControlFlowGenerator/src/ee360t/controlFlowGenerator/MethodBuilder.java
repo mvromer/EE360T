de4ea9bac8cd6c77fac52f966b50b9d526a72ca6
Michael Romer <mvromer@gmail.com>
2018-08-01 12:59:35 -0500

    Added skeleton analyzer

--- ControlFlowGenerator/src/ee360t/controlFlowGenerator/MethodBuilder.java
+++ ControlFlowGenerator/src/ee360t/controlFlowGenerator/MethodBuilder.java
@@ -1,6 +1,10 @@
 package ee360t.controlFlowGenerator;
 
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.Analyzer;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.objectweb.asm.tree.analysis.BasicInterpreter;
+import org.objectweb.asm.tree.analysis.BasicValue;
 
 import static org.objectweb.asm.Opcodes.ASM4;
 
@@ -15,7 +19,22 @@ public class MethodBuilder extends MethodNode {
 
     @Override
     public void visitEnd() {
-        // TODO: Build CFG using Analyzer.
+        // TODO: Build CFG using Analyzer.'
+        Analyzer<BasicValue> a = new Analyzer<BasicValue>( new BasicInterpreter() ) {
+            @Override
+            protected void newControlFlowEdge( int insnIndex, int successorIndex ) {
+                System.out.println( "edge from " + Integer.toString( insnIndex ) + " to " + Integer.toString( successorIndex ) );
+            }
+        };
+        try {
+            a.analyze( ownerClass.getName(), this );
+        }
+        catch( AnalyzerException ex ) {
+            // do nothing.
+            System.err.println( "Analyzer error" );
+            ex.printStackTrace( System.err );
+        }
+        System.out.println();
         ownerClass.addMethod( name, desc );
     }
 }
