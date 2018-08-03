4d96a3445b5a439680d346a9a1b4f2281b3abc74
Michael Romer <mvromer@gmail.com>
2018-08-02 22:10:44 -0500

    Added support for building pruned control flow graph (i.e., no labels etc)

--- ControlFlowGenerator/src/ee360t/controlFlowGenerator/MethodBuilder.java
+++ ControlFlowGenerator/src/ee360t/controlFlowGenerator/MethodBuilder.java
@@ -12,6 +12,7 @@ import org.objectweb.asm.util.TraceMethodVisitor;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,7 +29,17 @@ public class MethodBuilder extends MethodNode {
 
     @Override
     public void visitEnd() {
-        System.out.println( "Method " + name );
+        // ASM injects label, line number, and frame nodes in the instruction list, even though they aren't real
+        // instructions. We don't want those fake instructions in the control flow graph we use for this project, but Of
+        // course simply removing them prior to running the analyzer that builds the CFG would've been too simple.
+        //
+        // Instead, we build up the index graph and then do a pass on it after running the analyzer to prune out those
+        // nodes that correspond to the fake instructions we don't want. We don't bother rebasing indices, which has the
+        // advantage of being able to look up the real instruction nodes easily from our pruned index graph at a later
+        // time.
+        //
+
+        // Use a custom analyzer that will record the control flow edges found by the ASM method analyzer
         IndexGraph instructionGraph = new IndexGraph();
         Analyzer<BasicValue> a = new Analyzer<BasicValue>( new BasicInterpreter() ) {
             @Override
@@ -39,38 +50,47 @@ public class MethodBuilder extends MethodNode {
 
         try {
             a.analyze( ownerClass.getName(), this );
-            Map<Integer, Set<Integer>> edges = instructionGraph.getEdges();
-            for( int iFrom : edges.keySet() ) {
-                for( int iTo : edges.get( iFrom ) ) {
-                    System.out.println( "edge from " + iFrom + " to " + iTo );
+
+            printInstructions();
+            instructionGraph.printDot( System.out );
+
+            // Go through the list of instructions and remove nodes from our control flow graph corresponding to fake
+            // instructions signifying labels, line numbers, and stack frames.
+            int iInstruction = 0;
+            ListIterator<AbstractInsnNode> instructionIter = instructions.iterator();
+            while( instructionIter.hasNext() ) {
+                AbstractInsnNode currentInstruction = instructionIter.next();
+                int instructionType = currentInstruction.getType();
+
+                if( instructionType == AbstractInsnNode.LABEL ||
+                    instructionType == AbstractInsnNode.LINE ||
+                    instructionType == AbstractInsnNode.FRAME ) {
+                    System.out.println( "Removing instruction " + iInstruction );
+                    instructionGraph.removeNode( iInstruction );
                 }
+
+                ++iInstruction;
             }
+
+            instructionGraph.printDot( System.out );
         }
         catch( AnalyzerException ex ) {
             System.err.println( "Analyzer error" );
             ex.printStackTrace( System.err );
         }
 
-        System.out.println();
+        ownerClass.addMethod( name, desc );
+    }
 
-//        Textifier t = new Textifier();
-//        TraceMethodVisitor tmv = new TraceMethodVisitor( t );
-//        accept( tmv );
-//        StringWriter sw = new StringWriter();
-//        PrintWriter pw = new PrintWriter( sw );
-//        t.print( pw );
-//        pw.flush();
-        //System.out.println( sw.toString() );
+    private void printInstructions() {
         Textifier t = new Textifier();
         TraceMethodVisitor tmv = new TraceMethodVisitor( t );
-        for( AbstractInsnNode ain : instructions.toArray() ) {
-            ain.accept( tmv );
-            System.out.println( ain.getClass().toString() );
-        }
+        accept( tmv );
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter( sw );
         t.print( pw );
+        pw.flush();
+        System.out.println();
         System.out.println( sw.toString() );
-        ownerClass.addMethod( name, desc );
     }
 }
